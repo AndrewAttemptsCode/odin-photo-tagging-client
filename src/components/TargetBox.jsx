76b97df4973660b394acd9490322c16cd2463a96
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-21 15:49:37 +0100

    refactor: include target drop down menu and move marker into own component

--- src/components/TargetBox.jsx
+++ src/components/TargetBox.jsx
@@ -1,20 +1,13 @@
-import styled from "styled-components";
-
-const Box = styled.div`
-  position: absolute;
-  top: ${({$y}) => $y};
-  left: ${({$x}) => $x};
-  height: 20px;
-  width: 20px;
-  border: 2px solid red;
-  background-color: transparent;
-  transform: translate(-50%, -50%);
-`
+import TargetMarker from "./TargetMarker";
+import TargetDropdown from "./TargetDropdown";
 
 const TargetBox = ({ coords }) => {
   const { x, y } = coords;
   return (
-    <Box $x={`${x}px`} $y={`${y}px`}/>
+    <>
+    <TargetMarker x={x} y={y} />
+    <TargetDropdown x={x} y={y} />
+    </>
   );
 };
 
