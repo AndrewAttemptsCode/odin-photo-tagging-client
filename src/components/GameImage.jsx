1d3fdf6aa5949c72d79c2151873c45342a1b6ff5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-22 17:56:08 +0100

    feat: split pixels and normalized dimensions into their own states for visual and viewport logic

--- src/components/GameImage.jsx
+++ src/components/GameImage.jsx
@@ -7,35 +7,42 @@ const ImageContainer = styled.div`
   position: relative;
   height: auto;
   max-width: 900px;
-  border-radius: 10px;
-  background-color: aquamarine;
   cursor: crosshair;
 
   & img {
     object-fit: contain;
     width: 100%;
     height: 100%;
+    border-radius: 15px;
   }
 `
 
 const GameImage = () => {
   const [targetClick, setTargetClick] = useState(false);
-  const [coords, setCoords] = useState({x: null, y: null});
+  const [pixelCoords, setPixelCoords] = useState({x: 0, y: 0});
+  const [normalizedCoords, setNormalizedCoords] = useState({x: 0, y: 0});
 
   const handleClick = (event) => {
     const container = event.currentTarget.getBoundingClientRect();
 
     const x = event.clientX - container.left;
     const y = event.clientY - container.top;
+
+    const normalizedX = x / container.width;
+    const normalizedY = y / container.height;
+
+    console.log(`Normalized X: ${normalizedX}, normalized Y: ${normalizedY}`);
     console.log(`click: pos x: ${x} pos y: ${y}`);
-    setCoords({x, y});
+    
+    setPixelCoords({x, y});
+    setNormalizedCoords({x: normalizedX, y: normalizedY});
     setTargetClick(!targetClick);
   }
 
   return (
     <ImageContainer onClick={handleClick}>
       <img src={beach} alt="" />
-      {targetClick && <TargetBox coords={coords} />}
+      {targetClick && <TargetBox pixelCoords={pixelCoords} />}
     </ImageContainer>
   );
 };
