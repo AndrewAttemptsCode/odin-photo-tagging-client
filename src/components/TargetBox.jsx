235de0c429aedd6b9c769102aadf38a4a0de16c2
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-26 16:48:10 +0100

    feat: pass correct guesses prop

--- src/components/TargetBox.jsx
+++ src/components/TargetBox.jsx
@@ -1,12 +1,12 @@
 import TargetMarker from "./TargetMarker";
 import TargetDropdown from "./TargetDropdown";
 
-const TargetBox = ({ pixelCoords, handleGuess }) => {
+const TargetBox = ({ pixelCoords, handleGuess, correctGuesses }) => {
   const { x, y } = pixelCoords;
   return (
     <>
     <TargetMarker x={x} y={y} />
-    <TargetDropdown x={x} y={y} onGuess={handleGuess} />
+    <TargetDropdown x={x} y={y} onGuess={handleGuess} correctGuesses={correctGuesses} />
     </>
   );
 };
