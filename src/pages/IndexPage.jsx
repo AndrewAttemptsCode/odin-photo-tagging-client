8d2b4e77f658a8a2d10d8802d62521dfb1631c4d
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-27 17:22:57 +0100

    feat: lifted correct guess state so both image and game controls has access

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,15 +1,22 @@
 import styled from "styled-components";
 import GameImage from "../components/GameImage";
+import GameControls from "../components/GameControls";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
 `
 
 const IndexPage = () => {
+  const [correctGuesses, setCorrectGuesses] = useState([]);
+  
   return (
     <Container>
-      <GameImage />
+      <GameImage correctGuesses={correctGuesses} setCorrectGuesses={setCorrectGuesses} />
+      <GameControls correctGuesses={correctGuesses} />
     </Container>
   );
 };
