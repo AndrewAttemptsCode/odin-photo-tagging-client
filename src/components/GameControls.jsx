0fb479382d160ed7c252c0bbf031c9c462893298
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-28 14:38:53 +0100

    feat: add score counter component to the game controls component section

--- src/components/GameControls.jsx
+++ src/components/GameControls.jsx
@@ -1,18 +1,25 @@
 import styled from "styled-components";
 import GameKey from "./GameKey";
 import ItemsFound from "./ItemsFound";
+import ScoreCounter from "./ScoreCounter";
 
 const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
   max-width: 900px;
   border: 1px solid red;
 `
 
-const GameControls = ({ correctGuesses, guessOptions }) => {
+const GameControls = ({ correctGuesses, guessOptions, scoreCounter }) => {
   return (
     <Container>
-      <ItemsFound correctGuesses={correctGuesses} guessOptions={guessOptions} />
-      <GameKey correctGuesses={correctGuesses} guessOptions={guessOptions} />
+      <div>
+        <ItemsFound correctGuesses={correctGuesses} guessOptions={guessOptions} />
+        <GameKey correctGuesses={correctGuesses} guessOptions={guessOptions} />
+      </div>
+      <ScoreCounter counter={scoreCounter} />
     </Container>
   );
 };
