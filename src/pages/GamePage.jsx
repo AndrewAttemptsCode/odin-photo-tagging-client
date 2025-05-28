95cde2022f79f2428a88dc4630c853091c55b9eb
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-28 14:38:02 +0100

    feat: implement score tracking logic

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import GameImage from "../components/GameImage";
 import GameControls from "../components/GameControls";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import sludge from "../assets/sludge.webp";
 import ball from "../assets/ball.webp";
@@ -17,18 +17,31 @@ const Container = styled.div`
 
 const GamePage = () => {
   const [correctGuesses, setCorrectGuesses] = useState([]);
-  
-  const guessOptions = [
+  const [scoreCounter, setScoreCounter] = useState(0);
+
+  const guessOptions = useMemo(() => {
+    return [
     { name: "ball", label: "Ball", img: ball },
     { name: "sludge", label: "Sludge", img: sludge },
     { name: "ballboy", label: "Ball Boy", img: ballboy },
     { name: "inflatable", label: "Inflatable", img: inflatable },
   ];
+  }, []);
+  
+
+  useEffect(() => {
+    if (correctGuesses.length < guessOptions.length) {
+      const counter = setInterval(() => {
+      setScoreCounter(prev => prev + 1);
+    }, 1000);
+    return () => clearInterval(counter);
+    }
+  }, [correctGuesses, guessOptions]);
 
   return (
     <Container>
       <GameImage correctGuesses={correctGuesses} setCorrectGuesses={setCorrectGuesses} guessOptions={guessOptions} />
-      <GameControls correctGuesses={correctGuesses} guessOptions={guessOptions} />
+      <GameControls correctGuesses={correctGuesses} guessOptions={guessOptions} scoreCounter={scoreCounter} />
     </Container>
   );
 };
