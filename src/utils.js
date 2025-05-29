368d7d6ee7aa454f315581c66da2c3c016bf5457
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-29 18:59:06 +0100

    feat: converts date time to custom display

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+export const formatDate = (data) => {
+  const dateObj = new Date(data);
+  const date = String(dateObj.getDate()).padStart(2, '0');
+  const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+  const year = String(dateObj.getFullYear()).slice(-2);
+  return `${date}/${month}/${year}`;
+}
+
+export const formatTime = (data) => {
+  const dateObj = new Date(data);
+  const hours = String(dateObj.getHours()).padStart(2, '0');
+  const minutes = String(dateObj.getMinutes()).padStart(2, '0');
+  return `${hours}:${minutes}`;
+}
