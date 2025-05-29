86909f85946cdc7fd09d4c4104776f9e7b859da9
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-29 18:59:25 +0100

    feat: add leaderboard route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import GamePage from "./pages/GamePage";
+import LeaderboardPage from "./pages/LeaderboardPage";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { path: "game", element: <GamePage />}, 
+      { path: "game", element: <GamePage />},
+      { path: "leaderboard", element: <LeaderboardPage /> }, 
     ]
   },
 ]);
