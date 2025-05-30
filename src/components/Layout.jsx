a73966f2eb0cd9b18aa1ff821377f42e32347a30
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-30 19:25:28 +0100

    feat: add grid layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,10 +3,23 @@ import HeaderSection from "./HeaderSection";
 import styled from "styled-components";
 
 const Container = styled.div`
-  min-height: 100dvh;
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-areas: 
+    "header"
+    "main";
   min-height: 100vh;
+  min-height: 100dvh;
   background-color: #FFF8F0;
   color: #1E1E1E;
+
+  header {
+    grid-area: header;
+  }
+
+  main {
+    grid-area: main;
+  }
 `
 
 const Layout = () => {
