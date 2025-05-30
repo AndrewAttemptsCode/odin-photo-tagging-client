3627b308664d88bb75af18dee0569c65fe57a4bf
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-30 17:00:58 +0100

    feat: add logo and navbar components with responsive layout

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,9 +1,26 @@
+import styled from "styled-components";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #FF1D25;
+
+  @media (max-width: 480px) {
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+`
+
 const HeaderSection = () => {
   return (
-    <>
-      {/* logo
-      navbar */}
-    </>
+    <Container>
+      <Logo />
+      <Navbar />
+    </Container>
   );
 };
 
