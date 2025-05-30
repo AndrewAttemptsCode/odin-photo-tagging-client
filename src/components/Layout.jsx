07194c39d8f89c4ee60d80c7afda0ec616e9c717
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-30 17:03:19 +0100

    styles: add height control with background and text colours

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,24 @@
 import { Outlet } from "react-router-dom";
 import HeaderSection from "./HeaderSection";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: 100dvh;
+  min-height: 100vh;
+  background-color: #FFF8F0;
+  color: #1E1E1E;
+`
 
 const Layout = () => {
   return (
-    <div>
+    <Container>
       <header>
         <HeaderSection />
       </header>
       <main>
         <Outlet />
       </main>
-    </div>
+    </Container>
   );
 };
 
