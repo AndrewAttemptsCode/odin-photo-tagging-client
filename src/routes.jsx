b2690ffe8419a398ea05e2d7b1ac8c54e6505ceb
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-30 19:25:14 +0100

    feat: add homepage child element as index

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import GamePage from "./pages/GamePage";
 import LeaderboardPage from "./pages/LeaderboardPage";
+import HomePage from "./pages/HomePage";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
+      { path: "", element: <HomePage /> },
       { path: "game", element: <GamePage />},
       { path: "leaderboard", element: <LeaderboardPage /> }, 
     ]
